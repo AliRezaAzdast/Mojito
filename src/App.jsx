9ae10b1e105fcef6a1508889954bba40867dbd08
ali reza azdast <azdast47@gmail.com>
2025-06-22 22:25:22 +0330

    final commit of the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import gsap from 'gsap';
+import { ScrollTrigger} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-
-
   return (
     <>
-      <div className="overflow-hidden">
-          <h1>hellow</h1>
+      <div className='flex-center h-[100vh]'>
+          <h1 className='text-indigo-300'>hellow</h1>
       </div>
     </>
   );
