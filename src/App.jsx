106a1657e314ff5c117e2f2fd013c14f9c6aa902
ali reza azdast <azdast47@gmail.com>
2025-06-23 11:46:19 +0330

    finished a navbar and add constants folder to my constants data of my web site been using data folder but constants make more sence so ill use it from now on

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import gsap from 'gsap';
 import { ScrollTrigger} from "gsap/all";
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <>
-      <div className='flex-center h-[100vh]'>
-          <h1 className='text-indigo-300'>hellow</h1>
-      </div>
+      <main>
+          <Navbar/>
+      </main>
     </>
   );
 }
