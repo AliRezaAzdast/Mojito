2f340e7a3e8792578c2c75aef3e01f9215296236
ali reza azdast <azdast47@gmail.com>
2025-06-24 13:18:00 +0330

    finished the part of the hero section just animation of it remain

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import gsap from 'gsap';
 import { ScrollTrigger} from "gsap/all";
+import { SplitText } from "gsap/SplitText";
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
     <>
       <main>
           <Navbar/>
+          <Hero/>
+          <div className='h-dvh'>
+
+          </div>
       </main>
     </>
   );
