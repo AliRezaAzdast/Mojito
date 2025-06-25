cf1bea98d05deb727622fbcde2ba899d1db0577b
ali reza azdast <azdast47@gmail.com>
2025-06-25 12:41:07 +0330

    add 2 diffrent data to constants and added cocktails section to app cheaced animation still look amazing and added small animation to leaf of the contail section

--- constants/index.js
+++ constants/index.js
@@ -1,20 +1,74 @@
 const navLinks = [
- {
-	id: "cocktails",
-	title: "Cocktails",
- },
- {
-	id: "about",
-	title: "About Us",
- },
- {
-	id: "work",
-	title: "The Art",
- },
- {
-	id: "contact",
-	title: "Contact",
- },
+  {
+    id: "cocktails",
+    title: "Cocktails",
+  },
+  {
+    id: "about",
+    title: "About Us",
+  },
+  {
+    id: "work",
+    title: "The Art",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
-export {navLinks}
+const cocktailLists = [
+  {
+    name: "Chapel Hill Shiraz",
+    country: "AU",
+    detail: "Battle",
+    price: "$10",
+  },
+  {
+    name: "Caten Malbee",
+    country: "AU",
+    detail: "Battle",
+    price: "$49",
+  },
+  {
+    name: "Rhino Pale Ale",
+    country: "CA",
+    detail: "750 ml",
+    price: "$20",
+  },
+  {
+    name: "Irish Guinness",
+    country: "IE",
+    detail: "600 ml",
+    price: "$29",
+  },
+];
+
+const mockTailLists = [
+  {
+    name: "Tropical Bloom",
+    country: "US",
+    detail: "Battle",
+    price: "$10",
+  },
+  {
+    name: "Passionfruit Mint",
+    country: "US",
+    detail: "Battle",
+    price: "$49",
+  },
+  {
+    name: "Citrus Glow",
+    country: "CA",
+    detail: "750 ml",
+    price: "$20",
+  },
+  {
+    name: "Lavender Fizz",
+    country: "IE",
+    detail: "600 ml",
+    price: "$29",
+  },
+];
+
+export { navLinks, cocktailLists, mockTailLists };
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger} from "gsap/all";
 import { SplitText } from "gsap/SplitText";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,9 +13,7 @@ function App() {
       <main>
           <Navbar/>
           <Hero/>
-          <div className='h-dvh'>
-
-          </div>
+          <Cocktails/>
       </main>
     </>
   );
